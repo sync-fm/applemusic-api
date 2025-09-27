280010dbb61d499038419b9b9fb23b5c8f0a27d4
xwxfox <tobiasstaun26@gmail.com>
2025-09-27 17:59:45 +0200

    typecheck + eslint

---
+++ eslint.config.ts
@@ -0,0 +1,47 @@
+import pluginJs from '@eslint/js';
+import securityPlugin from 'eslint-plugin-security';
+import unicornPlugin from 'eslint-plugin-unicorn';
+import globals from 'globals';
+import tsPlugin from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    ignores: ['.next/**', 'node_modules/**', 'dist/**', 'coverage/**', 'scripts/**'],
+  },
+  // Security
+  //@ts-expect-error eslint-plugin-security has no types // not the "correct" ones at least
+  securityPlugin.configs.recommended,
+  {
+    files: ['**/*.ts'],
+  },
+  {
+    languageOptions: { globals: globals.node },
+  },
+  {
+    rules: {
+      'func-style': ['error', 'expression'],
+      'no-restricted-syntax': ['off', 'ForOfStatement'],
+      'prefer-template': 'error',
+    },
+  },
+  // TypeScript Eslint
+  {
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    },
+  },
+  // Unicorn
+  {
+    plugins: {
+      unicorn: unicornPlugin,
+    },
+    rules: {
+      'unicorn/empty-brace-spaces': 'off',
+      'unicorn/no-null': 'off',
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tsPlugin.configs.recommended,
+];
